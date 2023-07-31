82ba127cea1d391c86cc8709bb367c0fbbd6ea0c
ahmed walid <ahmed.walid605@yahoo.com>
2023-07-31 16:00:06 +0200

    Update Class.cpp

--- TestComponent/Class.cpp
+++ TestComponent/Class.cpp
@@ -6,11 +6,11 @@ namespace winrt::TestComponent::implementation
 {
     int32_t Class::MyProperty()
     {
-        throw hresult_not_implemented();
+
     }
 
     void Class::MyProperty(int32_t /* value */)
     {
-        throw hresult_not_implemented();
+
     }
 }
