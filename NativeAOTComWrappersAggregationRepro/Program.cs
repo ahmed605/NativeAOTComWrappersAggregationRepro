03f9c4ae7f7f7cc8524cdd9f679816b16b881a03
ahmed walid <ahmed.walid605@yahoo.com>
2023-07-31 13:27:47 +0200

    Add project files.

---
+++ NativeAOTComWrappersAggregationRepro/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using Windows.UI.Input;
+using Windows.UI.WindowManagement;
+
+namespace NativeAOTComWrappersAggregationRepro
+{
+    internal class TestClass : TestComponent.Class
+    {
+        internal TestClass() { Console.WriteLine("Hello!"); }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new TestClass();
+        }
+    }
+}
